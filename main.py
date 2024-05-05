5595fc8afd0b283ce9a05c255df3f04349d69c96
vitalik <malichkin@yandex.ru>
2024-05-05 23:25:40 +0300

    Tooday 05.04.2024

---
+++ main.py
@@ -0,0 +1,31 @@
+import telebot
+import requests
+import json
+
+bot = telebot.TeleBot('ТОКЕН ТВОГО БОТА')
+API = 'API сайта'
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,
+                     f'Привет, {message.from_user.first_name}. Рад тебя видеть! Напиши название города')
+
+
+@bot.message_handler(content_types=['text'])
+def get_weather(message):
+    city = message.text.strip().lower()
+    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API}&units=metric')
+    if res.status_code == 200:
+        data = json.loads(res.text)
+        temp = data["main"]["temp"]
+        bot.reply_to(message, f'Сейчас погода: {temp}')
+
+        image = 'sunny.jpeg.png' if temp > 5.0 else 'sunny2.jpeg'
+        file = open('./' + image, 'rb')
+        bot.send_photo(message.chat.id, file)
+    else:
+        bot.reply_to(message, f'Город указан не верно')
+
+
+bot.infinity_polling()
